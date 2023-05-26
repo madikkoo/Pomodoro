005e7d3de8d718ffd1c62b57a3b72d2995b329d2
Madiyar Niyazgali <mniyazgali@gmail.com>
2023-05-26 18:13:02 +0600

    upd

--- server.go
+++ server.go
@@ -6,13 +6,6 @@ import (
 )
 
 func main() {
-
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/about.html")
-	})
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/index.html")
-	})
 	fmt.Println("Server is listening...")
-	http.ListenAndServe(":8181", nil)
+	http.ListenAndServe(":8181", http.FileServer(http.Dir("static")))
 }
